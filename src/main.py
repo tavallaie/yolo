e1b623950c82a35e0c86e44c17e73a1d648bfa0f
Ali Tavallaie <a.tavallaie@gmail.com>
2021-04-03 18:24:37 +0430

    fix nameing

--- src/main.py
+++ src/main.py
@@ -13,51 +13,48 @@ app = FastAPI(
     version="0.0.1",
 )
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True,force_reload=True)
 
-# Images
-# dir = 'https://github.com/ultralytics/yolov5/raw/master/data/images/'
-# imgs = [dir + f for f in ('zidane.jpg', 'bus.jpg')]  # batched list of images
 
-# # Inference
-# results = model(imgs)
-# results.print()  # or .show(), .save()
 @app.post("/predict")
 async def serving(file:UploadFile=File(...)):
-    image = file.file
-    image = io.imread(image)
-    # print(type(image))
-    results = model(image)
-    r = {}
-    colors = color_list()
-    r['predict']={}
-    j = 0
-    for i, (img, pred) in enumerate(zip(results.imgs, results.pred)):
-        # str = f'image {i + 1}/{len(results.pred)}: {img.shape[0]}x{img.shape[1]} '
-        r["size"]= {"height":str(img.shape[0]),"width":str(img.shape[1])}
-        if pred is not None:
-            for c in pred[:, -1].unique():
-                n = (pred[:, -1] == c).sum()  # detections per class
-                
-                # str += f"{n} {results.names[int(c)]}{'s' * (n > 1)}, "
-                r['predict'][j]= results.names[int(c)]
-                j+=1
-        for *box, conf, cls in pred:  # xyxy, confidence, class
-                        label = f'{results.names[int(cls)]} {conf:.2f}'
-                        plot_one_box(box, img, label=label, color=colors[int(cls) % 10])
-        f= Path("data").mkdir(exist_ok=True)
-        f= Path("data") / "".join([results.names[int(c)],".jpeg"])
-        img = Image.fromarray(img.astype(np.uint8)) if isinstance(img, np.ndarray) else img 
+    try:    
+        image = file.file
+        image = io.imread(image)
+        results = model(image)
+        r = {}
+        colors = color_list()
+        r['result']={}
+        j = 0
 
-        # f = Path("data") / results.files[i] 
-        if f.is_file():
-            j = 1 
-            while(Path(f).is_file()):
-                f = Path("data") / "".join([results.names[int(c)],str(j),".jpeg"])
-                j+=1     
-        img.save(f)
-        r["image"]= "127.0.0.1:5000/"+str(f)
-    return {"result":r}
+        for i, (img, pred) in enumerate(zip(results.imgs, results.pred)):
+
+            r["size"]= {"height":str(img.shape[0]),"width":str(img.shape[1])}
+            if pred is not None:
+                for c in pred[:, -1].unique():
+                    n = (pred[:, -1] == c).sum()  # detections per class
+                    
+            for *box, conf, cls in pred:  # xyxy, confidence, class
+                            label = f'{results.names[int(cls)]} {conf:.2f}'
+                            plot_one_box(box, img, label=label, color=colors[int(cls) % 10])
+                            r['result'][j] ={"predictions":results.names[int(cls)],"conf":round(float(conf),3)}
+                            j+=1
+            f= Path("data").mkdir(exist_ok=True)
+            d = results.names[int(c)]
+            d = d.replace(" ", "_")
+            f= Path("data") / "".join([d,".jpeg"])
+            img = Image.fromarray(img.astype(np.uint8)) if isinstance(img, np.ndarray) else img 
+
+            if f.is_file():
+                j = 1 
+                while(Path(f).is_file()):
+                    f = Path("data") /"".join([d,str(j),".jpeg"])
+                    j+=1     
+            img.save(f)
+            r["image"]= "https://yolo.tavallaie.ir/"+str(f)
+        return r
+    except Exception as e:
+        return {"error":"some Error happend. please try with another image"}
 @app.get("/data/{name}")
 async def main(name):
     file_path = "data/{}".format(name)
